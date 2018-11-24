a3f8c829e0c0bea6dbb948794b1dbadb45863ae2
clyhs <clygd@126.com>
2018-11-24 21:40:29 +0800

    thymeleaf

---
+++ spring-boot-thymeleaf/src/main/java/org/abigfish/thymeleaf/App.java
@@ -0,0 +1,25 @@
+package org.abigfish.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App extends SpringBootServletInitializer
+{
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
+	
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    }
+}
