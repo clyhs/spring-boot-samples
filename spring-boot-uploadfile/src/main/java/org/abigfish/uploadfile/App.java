90314400f7635c8e104b55b230481a6251511f2e
clyhs <clygd@126.com>
2018-11-24 21:56:57 +0800

    uploadfile

---
+++ spring-boot-uploadfile/src/main/java/org/abigfish/uploadfile/App.java
@@ -0,0 +1,22 @@
+package org.abigfish.uploadfile;
+
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    }
+    
+   
+}
