8389e62b19100c66b1e109f4ba28264972d643ec
clyhs <clygd@126.com>
2018-11-28 11:30:05 +0800

    helloworld

--- spring-boot-helloworld/src/main/java/org/abigfish/helloworld/App.java
+++ spring-boot-helloworld/src/main/java/org/abigfish/helloworld/App.java
@@ -1,5 +1,8 @@
 package org.abigfish.helloworld;
 
+import org.abigfish.helloworld.properties.HomeProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +11,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class App 
+public class App implements CommandLineRunner
 {
+	@Autowired
+    private HomeProperties homeProperties;
+	
     public static void main( String[] args )
     {
     	SpringApplication.run(App.class, args);
     }
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("\n" + homeProperties.toString());
+        System.out.println();
+	}
 }
