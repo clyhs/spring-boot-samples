0e427b5573efc1f6d5433268fcde3764b3c8a432
clyhs <clygd@126.com>
2019-01-16 16:15:42 +0800

    ibatis ing

---
+++ spring-boot-ibatis/src/main/java/org/abigfish/ibatis/App.java
@@ -0,0 +1,13 @@
+package org.abigfish.ibatis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
