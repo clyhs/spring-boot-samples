763aa122b203e2a96857ab1a00c00fa3cc92baea
clyhs <clygd@126.com>
2019-07-04 17:16:49 +0800

    auth

--- spring-boot-2-oauth2-authorization-jwt/src/main/java/org/abigfish/oauth2/authorization/jwt/service/CustomUserDetailsService.java
+++ spring-boot-2-oauth2-authorization-jwt/src/main/java/org/abigfish/oauth2/authorization/jwt/service/CustomUserDetailsService.java
@@ -2,16 +2,21 @@ package org.abigfish.oauth2.authorization.jwt.service;
 
 import org.abigfish.oauth2.authorization.jwt.entity.User;
 import org.abigfish.oauth2.authorization.jwt.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AccountStatusUserDetailsChecker;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 
 @Service(value = "userDetailsService")
 public class CustomUserDetailsService implements UserDetailsService {
+
+	private static Logger log = LoggerFactory.getLogger(CustomUserDetailsService.class);
 
 	@Autowired
 	private UserRepository userRepository;
@@ -31,6 +36,15 @@ public class CustomUserDetailsService implements UserDetailsService {
 		new AccountStatusUserDetailsChecker().check(user);
 
 		return user;
+	}
+
+	public static void main(String[] args) {
+		PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		String encode = passwordEncoder.encode("password");
+		log.info("加密后的密码:" + encode);
+		log.info("bcrypt密码对比:" + passwordEncoder.matches("password", encode));
+		String md5Password = "{MD5}88e2d8cd1e92fd5544c8621508cd706b";// MD5加密前的密码为:password
+		log.info("MD5密码对比:" + passwordEncoder.matches("password", encode));
 	}
 
 }
