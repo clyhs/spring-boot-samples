6bd9601bccd8f15b19a2f10cd5f87c759f8a9370
clyhs <clygd@126.com>
2019-07-10 14:10:43 +0800

    docker

---
+++ spring-boot-docker/src/main/java/org/abigfish/docker/App.java
@@ -0,0 +1,19 @@
+package org.abigfish.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author abigfish
+ *
+ * @date 2019年7月10日
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    }
+}
